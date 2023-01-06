39cd3e7f7998039c48693e1e60a76d68219fe1b7
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-07 00:21:19 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { color } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 // import './style.css';
 const Home = () => {
   return (
     <>
-      <div className='main-page'>
-        <div className='main-content'>
-          <div className='main-heading'>
+      <div className="main-page">
+        <div className="main-content">
+          <div className="main-heading">
             <h2>
               DISCOVER SUPER
               <br />
               <span>NFTs</span>
             </h2>
           </div>
-          <div className='main-para'>
+          <div className="main-para">
             <p>
               At edgeXgame we are building a game collectable NFT marketplace
               with NFT based monthly subscription model. Players will be able to
@@ -22,19 +23,56 @@ const Home = () => {
               destroyed.
             </p>
           </div>
-          <div className='btn-container'>
-            <Link to='/buy' className='btn btn-buy'>
+          <div className="btn-container">
+            <Link to="/buy" className="btn btnhome btn-buy">
               Explore
             </Link>
-            <Link to='/aboutus' className='btn btn-buy'>
+            <Link to="/aboutus" className="btn btnhome btn-buy">
               Our Team
             </Link>
           </div>
         </div>
-        <div className='main-images'>
-          <div className='main-images-1'>
-            <img src='./images/nft_bg_1 (1).jpeg' alt='' className='img-main' />
-            <img src='./images/nft_2_bg (1).jpg' alt='' className='img-main' />
+        <div className="images">
+          <div className="left_image">
+            <img
+              src="https://c4.wallpaperflare.com/wallpaper/209/251/804/cod-mw-ghost-call-of-duty-hd-wallpaper-preview.jpg"
+              alt=""
+              className="img-main imgl"
+            />
+
+            <img
+              src="https://c4.wallpaperflare.com/wallpaper/741/914/473/machine-race-the-game-nfs-need-for-speed-heat-hd-wallpaper-preview.jpg"
+              alt=""
+              className="img-main imgl"
+            />
+          </div>
+
+          <div className="middle_image">
+            <img
+              src="https://c4.wallpaperflare.com/wallpaper/725/422/535/movies-hollywood-movies-wallpaper-preview.jpg"
+              alt=""
+              className="img-main imgm"
+            />
+          </div>
+
+          <div className="right_image">
+            <img
+              src="https://c4.wallpaperflare.com/wallpaper/837/704/915/video-game-fifa-19-neymar-soccer-wallpaper-preview.jpg"
+              alt=""
+              className="img-main imgr"
+            />
+
+            <img
+              src="https://c4.wallpaperflare.com/wallpaper/364/107/637/playerunknown-s-battlegrounds-explosion-artwork-pubg-wallpaper-thumb.jpg"
+              alt=""
+              className="img-main imgr"
+            />
+
+            <img
+              src="https://c4.wallpaperflare.com/wallpaper/107/848/913/spiderman-ps4-spiderman-games-hd-wallpaper-preview.jpg"
+              alt=""
+              className="img-main imgr"
+            />
           </div>
         </div>
       </div>
