29a87f125b40eaf98d306908397ddc0131351626
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-07 00:22:46 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,56 +1,67 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [walletAddress, setWalletAddress] = useState('');
+  const [walletAddress, setWalletAddress] = useState("");
   const walletConnect = async () => {
-    console.log('Requesting Account');
+    console.log("Requesting Account");
 
     //check if metamsk extention exist
     if (window.ethereum) {
-      console.log('detected');
+      console.log("detected");
 
       try {
         const accounts = await window.ethereum.request({
-          method: 'eth_requestAccounts',
+          method: "eth_requestAccounts",
         });
         setWalletAddress(accounts[0]);
       } catch (error) {
-        console.log('Error Connecting ...');
+        console.log("Error Connecting ...");
       }
     } else {
-      console.log('Metamask not detected');
+      console.log("Metamask not detected");
     }
   };
   return (
     <>
-      <nav className='navbar'>
-        <h3>edgexgame</h3>
-        <ul>
-          <li>
-            <Link to='/' className='nav-link'>
+      <nav className="navbar">
+        <div className="left_nav">
+        <Link to="/" className="nav-link">
+          <img
+            className="logo"
+            src={require("./edgeXgame-logos_transparent.png")}
+            alt=""
+          />
+          </Link>
+        </div>
+        <div className="right_nav">
+          <button className="home btn">
+            <Link to="/" className="nav-link">
               Home
             </Link>
-          </li>
-          <li>
-            <Link to='/buy' className='nav-link'>
+          </button>
+          <button className="buy btn">
+            <Link to="/buy" className="nav-link">
               Buy
             </Link>
-          </li>
-          <li>
-            <Link to='/aboutus' className='nav-link'>
-              About
+          </button>
+          <button className="about btn">
+            <Link to="/aboutus" className="nav-link">
+              About Us
             </Link>
-          </li>
-          <li>
-            <button className='btn nav-btn' onClick={walletConnect}>
-              {walletAddress ? walletAddress : 'Connect Wallet'}
-            </button>
-          </li>
-        </ul>
+          </button>
+          <button
+            className="connect_wallet btn_connect_wallet"
+            onClick={walletConnect}
+          >
+            {walletAddress ? walletAddress : "Connect Wallet"}
+          </button>
+        </div>
       </nav>
     </>
   );
 };
 
 export default Navbar;
+
+
