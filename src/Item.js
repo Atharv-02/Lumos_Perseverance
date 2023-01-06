0556045f8643376aaa4fff7aeaf036a860423b90
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-07 00:22:07 +0530

    Update Item.js

--- src/Item.js
+++ src/Item.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
-import games from './data';
-import { ethers } from 'ethers';
-import './style.css';
-import './nft-marketplace.json';
+import React, { useState } from "react";
+import games from "./data";
+import { ethers } from "ethers";
+import "./style.css";
+import "./nft-marketplace.json";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Text,
+  Heading,
+  Stack,
+  Divider,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
 
-const allCategories = ['all', ...new Set(games.map((game) => game.category))];
+const allCategories = ["all", ...new Set(games.map((game) => game.category))];
 console.log(allCategories);
 
 const Items = () => {
-  const contractABI = require('./nft-marketplace.json');
-  const YOUR_CONTRACT_ADDRESS = '0xB692fD2Cf5ddd28C459D68FF4503d41c8F297E59';
+  const contractABI = require("./nft-marketplace.json");
+  const YOUR_CONTRACT_ADDRESS = "0xB692fD2Cf5ddd28C459D68FF4503d41c8F297E59";
   const [gamesList, setGamesList] = useState(games);
   const [categories, setCategories] = useState(allCategories);
   const [count, setCount] = useState(3);
@@ -30,13 +43,13 @@ const Items = () => {
     // setCount(count + 1);
     await getContract().registerNft(title, desc, price);
     const tx = await getContract().buy(count);
-    alert('Once block is mined, Value will be auto updated');
+    alert("Once block is mined, Value will be auto updated");
     // await tx.wait();
     // fetchCurrentValue();
   };
 
   const filterItems = (category) => {
-    if (category === 'all') {
+    if (category === "all") {
       setGamesList(games);
     } else {
       console.log(category);
@@ -50,50 +63,60 @@ const Items = () => {
   };
 
   let listenToEvent = async () => {
-    getContract().on('counterIncremented', async (sender, value, event) => {
+    getContract().on("counterIncremented", async (sender, value, event) => {
       // Called when anyone changes the value
     });
   };
 
   return (
     <>
-      <section className='buy-page'>
-        <div className='items-btn-container'>
-          <button className='btn btn-menu' onClick={() => filterItems('all')}>
+      <section className="buy-page">
+        <div className="items-btn-container">
+          <button className="btn btn-menu" onClick={() => filterItems("all")}>
             All
           </button>
           <button
-            className='btn btn-menu'
-            onClick={() => filterItems('E-Sports')}
+            className="btn btn-menu"
+            onClick={() => filterItems("E-Sports")}
           >
             E Sports
           </button>
           <button
-            className='btn btn-menu'
-            onClick={() => filterItems('Racing')}
+            className="btn btn-menu"
+            onClick={() => filterItems("Racing")}
           >
             Racing
           </button>
         </div>
-        <div className='games-list'>
+        <div className="games-list">
           {gamesList.map((game) => {
             const { id, img, name, price, description } = game;
             return (
-              <div className='game' key={id}>
-                <div className='game-img'>
-                  <img src={img} alt='' />
-                </div>
-                <div className='game-text'>
-                  <h3>{name}</h3>
-                  <p>{price} MATIC</p>
-                  <p>{description}</p>
-                  <button
-                    className='btn game-btn'
-                    onClick={() => incrementCount(name, description, price)}
-                  >
-                    BUY NOW
-                  </button>
-                </div>
+              <div className="carddd">
+              <Card maxW="sm">
+                <CardBody>
+                  <Heading size="md" color="white">{name}</Heading>
+                  <Image className="card_img" src={img} alt="" borderRadius="lg" />
+                  <Stack mt="6" spacing="3">
+                    <Heading size="md" color="white">{name}</Heading>
+                    <Text color="white">{description}</Text>
+                    <Text color="black" fontSize="2xl">
+                      Price : {price} MATIC
+                    </Text>
+                  </Stack>
+                </CardBody>
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <Button
+                      variant="solid"
+                      colorScheme='green'
+                      onClick={() => incrementCount(name, description, price)}
+                    >
+                      Buy now
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
               </div>
             );
           })}
